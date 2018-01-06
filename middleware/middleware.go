79d9186f2f6058876e18033caaa402c4c9819709
cnt0 <sarum9n@gmail.com>
2018-01-06 10:49:38 +0500

    add mutex to IfModifiedSince

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,18 +2,22 @@ package middleware
 
 import (
 	"net/http"
+	"sync"
 	"time"
 )
 
 // IfModifiedSince uses "If-Modified-Since" and "Last-Modified" http headers
-// to optimize http requests. Obviously not a thread-safe
+// to optimize http requests.
 type IfModifiedSince struct {
+	m sync.Mutex
 	t time.Time
 }
 
 // Handler adds "If-Modified-Since" header to the given request
 func (i *IfModifiedSince) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		i.m.Lock()
+		defer i.m.Unlock()
 		r.Header.Set("If-Modified-Since", i.t.Format(http.TimeFormat))
 		next.ServeHTTP(w, r)
 		if t, err := time.Parse(http.TimeFormat, w.Header().Get("Last-Modified")); err == nil {
