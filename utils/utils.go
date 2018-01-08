d735188e06cc402ab1691e6c2c57ae0b0b36dabd
cnt0 <sarum9n@gmail.com>
2018-01-08 12:27:14 +0500

    add socket activation listener

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"net"
 	"net/http"
+	"os"
+	"strconv"
 	"sync"
 )
+
+// defined in src/activate/activate.c from systemd source tree
+const sdListenFDsStart = 3
 
 // HTTPResponseWithErr is just what it is
 type HTTPResponseWithErr struct {
@@ -25,4 +31,19 @@ func ParallelRequests(reqs []*http.Request, client *http.Client) []*HTTPResponse
 	}
 	wg.Wait()
 	return resps
+}
+
+// ListenAndServeSA listens on unix socket provided by systemd socket activation.
+// If there is no socket activation, it will act just like http.ListenAndServe
+func ListenAndServeSA(addr string, handler http.Handler) error {
+	if os.Getenv("LISTEN_PID") == strconv.Itoa(os.Getpid()) {
+		l, err := net.FileListener(os.NewFile(sdListenFDsStart, "socket"))
+		if err != nil {
+			return err
+		}
+		if err := http.Serve(l, handler); err != nil {
+			return err
+		}
+	}
+	return http.ListenAndServe(addr, handler)
 }
